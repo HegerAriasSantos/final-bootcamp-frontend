63497e628388c8ebcf8b3076f3386d055894f73b
Larison <larison.sepulveda01@gmail.com>
2022-08-06 23:46:02 -0400

    Larison's update, adding main home/style/index to the project to iniciate the home page

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,5 +1,24 @@
+import "./styles.css";
 const Navbar = () => {
-  return <h1>navbar</h1>;
+  return(
+    <>
+    
+    <div id="buttons">
+<nav>
+    <picture> <img src="#" alt="#" /> </picture>
+			<div className='nav1'>
+				  <a href="#">Home</a>
+				  <a href="#">Dates</a>
+			</div>
+    <div className="nav2">
+        <a href="#">Login</a>
+			  <a href="#">Register</a>
+    </div>
+</nav>
+    </div>
+
+    </>
+  );
 };
 
 export default Navbar;
--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,7 +1,7 @@
-import Hello from '~/components/Hello';
-
+// import Hello from '~/components/Hello';
+import Navbar from '~/components/Navbar';
 function App() {
-  return <Hello />;
+  return <Navbar />;
 }
 
 export default App;
