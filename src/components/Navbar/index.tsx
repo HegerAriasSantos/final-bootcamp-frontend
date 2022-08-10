4eb1f1a169a21c7e2b9f2dcfb6c0287126504cb3
Larison <larison.sepulveda01@gmail.com>
2022-08-10 19:24:09 -0400

    update of the main page v0.2

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,23 +1,28 @@
-import "./styles.css";
+import "./styles.scss";
+import Logo from '../../assets/img/barbero.jpeg'
 const Navbar = () => {
   return(
-    <div className="navbar-complete">
-    
     <div>
-<nav>
-    <picture> <img src="" alt="#" /> </picture>
-			<div>
+    <nav>
+        <input type="checkbox" id="check"></input>
+        <label className="checkbtn">
+            <i className="fas fa-bars"></i>
+        </label>
+        <a href="#" className="enlace">
+
+            <img src={Logo} className="logo"/>
+        </a>
         <ul>
-				  <li><a href="#" className="navbarHome">Home</a></li>
-          <li> <a href="#" className="navbarDates">Dates</a></li>
-          <li> <a href="#">Login</a></li>
-          <li> <a href="#">Register</a></li>
+            <li><a className="active" href="#">Home</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Login</a></li>
+            <li><a href="#">Register</a></li>
+            <li><a href="#">About us</a></li>
         </ul>
-    </div>
-</nav>
-    </div>
+    </nav>
+
     </div>
   );
-};
+}
 
 export default Navbar;
--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -4,14 +4,60 @@ function App() {
   return (
     <>
       <Navbar/>
-    <div>
-      <h1>Welcome to the best barber shop of all times!</h1>
-    </div>
-
-    <footer>
-    <h3> Copyright design license </h3>
-    </footer>
       
+  <body style={{backgroundColor: '#e8e8e8',
+                padding: '2px',
+                margin: '0',
+                fontFamily: 'monospace'
+
+
+}}>
+  <br/><br/>
+    <div>
+      <h1 style={{
+        padding: '0',
+        height: '100%',
+      }}>You need to look good and feel good!</h1><br/>
+
+        {/* This is the style of the description */}
+      <p style={{
+        fontSize: '15px',
+
+      }}>This is your opportunity! Join us and have the best experience of your life!</p><br/><br/>
+
+    </div>
+    
+    {/* THIS IS HE PICTURE */}
+      <img src="https://thefader-res.cloudinary.com/private_images/c_limit,w_1024/c_crop,h_683,w_1025,x_0,y_75,f_auto,q_auto:eco/IMG_1928_WEB_havya5/IMG_1928_WEB_havya5.jpg" alt="Foto" style={{width: '50%', height: '50%'}} /><br/><br/>
+    {/* THIS IS HE BUTTON */}
+      <div>
+        <button type="button" style={{color: 'black',
+        padding: '0.7em 1.7em',
+        fontSize: '18px',
+        borderRadius: '0.5em',
+        background: '#4b75ff',
+        border: '1px solid #e8e8e8',
+        transition: 'all .4s',
+        boxShadow: '6px 6px 12px #c5c5c5, -6px -6px 12px #fff',
+        alignItems: 'left'
+      }}> Contact us!</button> <br /><br />
+
+      </div>
+      {/* THIS IS A TEST */}
+      
+      {/* THIS IS THE FOOTER */}
+    <footer>
+    <h3 style={{
+      height: '50px', 
+      background:'#BDBDBD',  
+      width:'100%',
+      position: 'sticky', 
+      bottom:0,
+      color:'black'
+    }}> Copyright design license by react group </h3>
+    </footer>
+
+    </body>
     </>
   );
 }
