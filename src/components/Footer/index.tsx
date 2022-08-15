7ce58ab474b2f328a5ee5e8cf8787920cb442152
RonnyAraujoTest <102432892+RonnyAraujoTest@users.noreply.github.com>
2022-08-14 20:00:54 -0400

    create component Footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,10 @@
+import "./styles.scss"
+function Footer() {
+    return(
+        <footer className="footer">
+            <p>&copy;Copyright Desing by Joseadores Anonimos 2022.</p>
+        </footer>
+    )
+}
+
+export default Footer;
