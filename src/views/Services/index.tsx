15b7b31672a584058d8a650fc715c6eadfc0a1ac
RonnyAraujoTest <102432892+RonnyAraujoTest@users.noreply.github.com>
2022-08-18 07:37:53 -0400

    add carousel the services

--- src/views/Services/index.tsx
+++ src/views/Services/index.tsx
@@ -1,57 +1,60 @@
-//import Boy from '~/assets/img/2.png'
-import { useEffect, useState } from 'preact/hooks';
 import silla from '~/assets/img/barbero-png.png'
-import Axios from 'axios';
-//import Degradado2 from '~/assets/img/3.png'
-// Degradado3 from '~/assets/img/4.png'
 import './index.scss';
+import Carousel from 'react-bootstrap/Carousel';
 
-const [serviceList, setServiceList] = useState();
-// useEffect(()=>{
-  
-// })
+
+const CarouselService = () =>{
+  let services = [
+    {
+      "name": "Recortada de Barba",
+      "price": 150
+    },
+    {
+      "name": "Recorada Completa",
+      "price": 400
+    }
+  ]
+
+  return(
+    <Carousel controls={false} indicators={false}>
+      {services.map((item, index) => {
+        return(
+          <Carousel.Item>
+            <div className="new">
+              <p>Offer #{index}</p>
+            </div>
+            <div className="servicio2">
+              <div className="info-service">
+                <p>{item.name}</p>
+                <p><span>${item.price}.00</span></p>
+              </div>
+            </div>
+          </Carousel.Item>
+        )
+      })}
+    </Carousel>
+ )
+}
 
 const Services = () => {
   return (
     <div className="services-contain">
 
-
       <div className="section1">
-      <div className="div-servicio">
-          <div className="new">
-              <p>Offer</p>
-            </div>
-            <div className="servicio2">
-          
-            {/* <img src={Degradado} alt="" /> */}
-            <div className="info-service">
-              <p>Name Corte</p>
-              <p><span>$000.00</span></p>
-            </div>
-            </div>
-          </div>
+        <CarouselService/>
       </div>
-
-
 
       <div className="section2">
         <div className="contenido">
           <div className="shadow">
             <img src={silla} alt="" />
-          <h2> "Available services"</h2>
-              <p>Barbershop</p>
-
+            <h2> "Available services"</h2>
+            <p>Barbershop</p>
           </div>
         </div>
-
       </div>
     </div>
   );
-}
-
-
-const ServicesCart = () =>{
-
 }
 
 export default Services;
